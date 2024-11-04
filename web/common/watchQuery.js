e3894690625d686daee764647b9b1f3bea3d02f1
cbdyzj <cbdyzj@gmail.com>
2024-11-04 08:48:59 +0800

    Amend

--- web/common/watchQuery.js
+++ web/common/watchQuery.js
@@ -1,6 +1,8 @@
 import { watch } from 'vue'
+import { useRouter, route } from 'vue-router'
 
 export function useWatchQuery(store, keyList = []) {
+    const router = useRouter()
     for (const key of keyList) {
         watch(() => store[key], (value) => {
             if (route.query[key] !== value) {
