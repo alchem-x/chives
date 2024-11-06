3582455bc16cf136332d2601dfd728c48e514d87
cbdyzj <cbdyzj@gmail.com>
2024-11-06 19:58:51 +0800

    Amend

--- server/services/watch.js
+++ server/services/watch.js
@@ -1,31 +1,32 @@
+import { Cron } from 'croner'
 import { getRealtimeStock } from './snowball.js'
 import { database } from './database.js'
 import { sendBarkNotice } from './bark.js'
 
-async function pollWatch() {
-    while (true) {
-        for (const it of database.data.watchList) {
-            if (it.enabled) {
-                const r = await getRealtimeStock(it.symbol)
-                const data = r?.data?.[0]
-                if (data) {
-                    if ((it.type === 'UP_TO' && data.current >= it.price)) {
-                        sendBarkNotice(it.barkAPI, `⏰${it.name ?? ''}${it.symbol}价格涨到${data.current}${it.comment ? `/${it.comment}` : ''}`)
-                        it.enabled = false
-                        await database.write()
-                    }
-                    if ((it.type === 'DOWN_TO' && data.current <= it.price)) {
-                        sendBarkNotice(it.barkAPI, `⏰${it.name ?? ''}${it.symbol}价格跌到${data.current}${it.comment ? `/${it.comment}` : ''}`)
-                        it.enabled = false
-                        await database.write()
-                    }
+async function watchStockPrice() {
+    console.log('watchStockPrice', new Date())
+    for (const it of database.data.watchList) {
+        if (it.enabled) {
+            const r = await getRealtimeStock(it.symbol)
+            const data = r?.data?.[0]
+            if (data) {
+                if ((it.type === 'UP_TO' && data.current >= it.price)) {
+                    sendBarkNotice(it.barkAPI, `⏰${it.name ?? ''}${it.symbol}价格涨到${data.current}${it.comment ? `/${it.comment}` : ''}`)
+                    it.enabled = false
+                    await database.write()
+                }
+                if ((it.type === 'DOWN_TO' && data.current <= it.price)) {
+                    sendBarkNotice(it.barkAPI, `⏰${it.name ?? ''}${it.symbol}价格跌到${data.current}${it.comment ? `/${it.comment}` : ''}`)
+                    it.enabled = false
+                    await database.write()
                 }
             }
         }
-        await new Promise((resolve) => setTimeout(resolve, 1000))
     }
 }
 
 export function startWatch() {
-    const _ = pollWatch()
+    const job = new Cron('*/2 * * * * *', async () => {
+        await watchStockPrice()
+    })
 }
