8a4d064e73102ae4b5c450714856c33861e41c3f
cbdyzj <cbdyzj@gmail.com>
2024-11-07 20:58:30 +0800

    Amend

--- server/common/global.js
+++ server/common/global.js
@@ -2,6 +2,7 @@ import { config } from 'dotenv'
 
 config()
 
+export const DB_TOKEN = process.env.DB_TOKEN
 export const APP_PORT = process.env.PORT ?? 3000
 export const SNOWBALL_TOKEN = process.env.SNOWBALL_TOKEN
 export const CW_TOKEN = process.env.CW_TOKEN
