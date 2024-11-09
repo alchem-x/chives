708329e440abe1d2da4096c249f309e8d7f664a9
cbdyzj <cbdyzj@gmail.com>
2024-11-09 18:38:53 +0800

    Amend

--- server/common/global.js
+++ server/common/global.js
@@ -4,6 +4,7 @@ const { parsed } = config()
 
 export const {
     APP_PORT = 3000,
+    RESEND_MAIL,
     SNOWBALL_TOKEN,
     CW_TOKEN, 
     BARK_API,
--- server/services/resend.js
+++ server/services/resend.js
@@ -1,3 +1,26 @@
-export async function sendMail() {
+import { Resend } from 'resend'
+import { RESEND_MAIL } from '../common/global.js'
 
+const resend = new Resend(RESEND_MAIL)
+
+export async function sendMail({ to, subject, html }) {
+    const { data, error } = await resend.emails.send({
+        from: '韭菜观察 <cw@nano-exp.com>',
+        to,
+        subject,
+        html,
+    })
+    if (error) {
+        throw new Error(error)
+    } else {
+        return data
+    }
 }
+
+/*
+await sendMail({
+    to: ['cbdyzj@gmail.com'],
+    subject: 'Subject',
+    html: 'HTML content'
+})
+*/
